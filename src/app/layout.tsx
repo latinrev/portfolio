e83d4d632a0dc2f5515c985c3eb06fc6cf2c3612
latinrev <latinreve@gmail.com>
2024-09-06 20:32:50 -0500

    add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import "./globals.css";
-
+import { Analytics } from "@vercel/analytics/react";
 export const metadata = {
   title: "Joel Castillo",
   description: "I'm Joel Castillo a Full stack web developer based on the US, inspired by minimalism and creating proudcts that connect the world",
@@ -8,6 +8,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <Analytics/>
       <body>{children}</body>
     </html>
   );
